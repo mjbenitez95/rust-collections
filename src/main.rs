7aebc560b606c292a6bc7458f79090d11bd7d781
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 16:36:24 -0800

    reorganize main

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ enum SpreadsheetCell {
 }
 
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    access_vector();
+    mutate_vector();
+    iterate_and_mutate_vector();
+    introduce_enum_vector();
+}
+
+fn access_vector() {
+    let v = vec![1, 2, 3, 4, 5];
     let third: &i32 = &v[2];
 
     println!("The third element of our vector is {}!", third);
@@ -15,15 +22,20 @@ fn main() {
         Some(third) => println!("The third element of our vector is {}!", third),
         None => println!("Our vector has no third element."),
     }
+}
+
+fn mutate_vector() {
+    let mut v = vec![1, 2, 3, 4, 5];
 
     // let does_not_exist = &v[100]; // this line attempts to access out of bounds, causing a runtime crash
     let _does_not_exist = v.get(100); // whereas this line just returns an Option<T> of None
 
-    let first = &v[0];
+    let first = &v[0]; // <- this is an immutable borrow
     println!("The first element of our vector is {}!", first);
-    v.push(6);
-    // if we tried to access first after this, we'd have an issue with a mutable reference, and won't compile
+    v.push(6); // <- this is a mutable borrow, so we can't have an immutable one after
+}
 
+fn iterate_and_mutate_vector() {
     let v2 = vec![100, 32, 57];
     for i in &v2 {
         println!("Vector 2 has element {}!", i);
@@ -34,7 +46,9 @@ fn main() {
         *i += 50; // note the usage of the dereference operator (*)
         println!("New vector 2 has element {}!", i);
     }
+}
 
+fn introduce_enum_vector() {
     // we can use enums to put objects of different known types into a vector
     let row = vec![
         SpreadsheetCell::Int(3),
